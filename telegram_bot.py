685e6854bb4839abe82f2a58404046cb469ccd83
RedJJJak <144244991+RedJJJak@users.noreply.github.com>
2025-03-03 11:24:47 +0000

    Update telegram_bot.py

--- telegram_bot.py
+++ telegram_bot.py
@@ -1,8 +1,6 @@
 import logging
 import requests
 import os
-from keep_alive import keep_alive
-keep_alive()
 from telegram import Update, InlineKeyboardMarkup, InlineKeyboardButton, ReplyKeyboardRemove
 from telegram.ext import (
     Application,
